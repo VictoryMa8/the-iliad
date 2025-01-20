f88f579c422564f7eaec254730656034047e0444
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-01-20 10:45:55 -0600

    scss stuff

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import bootstrap from "bootstrap";
+const viewButtons = document.querySelectorAll(".viewbutton");
+const overlay = document.querySelector(".overlay");
+const overlayImage = document.querySelector(".overlay__inner img");
+const closeButton = document.querySelector(".close");
+
+function open(e) {
+  e.preventDefault(); // Prevent any default button behavior
+  e.stopPropagation(); // Stop event from bubbling up
+  overlay.classList.add("open");
+  // Go up to the project div and find its image
+  const projectImage = e.currentTarget
+    .closest(".project")
+    .querySelector(".project__image");
+  overlayImage.src = projectImage.src;
+}
+
+function close(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  overlay.classList.remove("open");
+}
+
+viewButtons.forEach((button) => button.addEventListener("click", open));
+closeButton.addEventListener("click", close);
+// Only close when clicking the overlay background, not its children
+overlay.addEventListener("click", function (e) {
+  if (e.target === overlay) {
+    close(e);
+  }
+});
